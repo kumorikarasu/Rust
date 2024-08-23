7135de980329d3c635ffa76d68c51289cc37d180
Sean Stacey <kumori@ryougi.com>
2024-08-23 11:55:51 -0400

    Solved question

--- dataquestion/src/in_memory_event_aggregator.rs
+++ dataquestion/src/in_memory_event_aggregator.rs
@@ -19,10 +19,33 @@ impl InMemoryEventAggregator {
 
 impl EventAggregator for InMemoryEventAggregator {
     fn collect_event(&mut self, event: crate::event::Event) {
-        todo!();
+        let key = event.name.clone() + &event.event_name;
+
+        let timestamp = self.timestamps.entry(key.clone()).or_insert(Vec::new());
+
+        if ! timestamp.contains(&event.timestamp_millis) {
+            timestamp.push(event.timestamp_millis);
+        }
+
+        let aggregation = self.result.entry(key).or_insert(AggregationResult {
+            name: event.name.clone(),
+            event_name: event.event_name.clone(),
+            total_events: 0,
+            earlist_timestamp_millis: std::u64::MAX,
+            latest_timestamp_millis: 0,
+            unique_timestamps: 0
+        });
+
+        aggregation.total_events += 1;
+        aggregation.earlist_timestamp_millis = std::cmp::min(aggregation.earlist_timestamp_millis, event.timestamp_millis);
+        aggregation.latest_timestamp_millis = std::cmp::max(aggregation.latest_timestamp_millis, event.timestamp_millis);
+        aggregation.unique_timestamps = timestamp.len() as u64;
+
+        println!("Aggregation: {:?}", aggregation);
     }
 
     fn get_aggregation_result(&self, name: &str, event_name: &str) -> Option<crate::aggregation_result::AggregationResult> {
-        todo!();
+        let key = name.to_string() + event_name;
+        self.result.get(&key).map(|x| x.clone())
     }
 }
